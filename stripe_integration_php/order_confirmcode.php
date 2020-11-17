4189091f3bd0675e1c3678dd7ca04095c0c07861
izenish <50284866+izenish@users.noreply.github.com>
2020-11-17 14:19:11 +0545

    del feture amended

--- stripe_integration_php/order_confirmcode.php
+++ stripe_integration_php/order_confirmcode.php
@@ -49,7 +49,7 @@ mysqli_close($conn);
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Bootstrap Example</title>
+  <title>Confirmation Code</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
